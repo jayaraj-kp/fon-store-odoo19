05787921b3deb3b2aa2d4fa9988b2c24e569084b
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-02-28 22:49:30 +0530

    custom

--- warehouse_user_restriction/models/purchase_order.py
+++ warehouse_user_restriction/models/purchase_order.py
@@ -11,58 +11,21 @@ class PurchaseOrder(models.Model):
         if user._is_superuser():
             return super()._search(domain, offset=offset, limit=limit, order=order, **kwargs)
         if user.allowed_warehouse_ids:
-            allowed_wh_ids = user.allowed_warehouse_ids.ids
             domain = list(domain) + [
-                ('picking_type_id.warehouse_id', 'in', allowed_wh_ids)
+                ('picking_type_id.warehouse_id', 'in', user.allowed_warehouse_ids.ids)
             ]
         return super()._search(domain, offset=offset, limit=limit, order=order, **kwargs)
 
-    @api.onchange('picking_type_id')
-    def _onchange_picking_type_id_warehouse_check(self):
-        """Filter the Deliver To dropdown to only allowed warehouses."""
+    @api.model
+    def default_get(self, fields_list):
+        """Set default Deliver To based on user's allowed warehouse."""
+        res = super().default_get(fields_list)
         user = self.env.user
-        if user.allowed_warehouse_ids:
-            allowed_wh_ids = user.allowed_warehouse_ids.ids
-            return {
-                'domain': {
-                    'picking_type_id': [
-                        ('warehouse_id', 'in', allowed_wh_ids),
-                        ('code', '=', 'incoming'),
-                    ]
-                }
-            }
-
-
-class PurchaseOrderInheritView(models.Model):
-    """Separate model to handle the view domain injection via _get_default."""
-    _inherit = 'purchase.order'
-
-    allowed_picking_type_domain = fields.Many2many(
-        'stock.picking.type',
-        string='Allowed Picking Type Domain',
-        compute='_compute_allowed_picking_type_domain',
-    )
-
-    @api.depends_context('uid')
-    def _compute_allowed_picking_type_domain(self):
-        user = self.env.user
-        if user.allowed_warehouse_ids:
-            allowed = self.env['stock.picking.type'].search([
+        if 'picking_type_id' in fields_list and user.allowed_warehouse_ids:
+            picking_type = self.env['stock.picking.type'].search([
                 ('warehouse_id', 'in', user.allowed_warehouse_ids.ids),
                 ('code', '=', 'incoming'),
-            ])
-        else:
-            allowed = self.env['stock.picking.type'].search([
-                ('code', '=', 'incoming'),
-            ])
-        for record in self:
-            record.allowed_picking_type_domain = allowed
-
-    def _get_default_picking_type_id_domain(self):
-        user = self.env.user
-        if user.allowed_warehouse_ids:
-            return [
-                ('warehouse_id', 'in', user.allowed_warehouse_ids.ids),
-                ('code', '=', 'incoming'),
-            ]
-        return [('code', '=', 'incoming')]
+            ], limit=1)
+            if picking_type:
+                res['picking_type_id'] = picking_type.id
+        return res
