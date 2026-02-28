c771f7d7a4987bcb371c655c1230d5f2a04eccd9
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-02-28 23:21:36 +0530

    custom

--- warehouse_user_restriction/models/pos_config.py
+++ warehouse_user_restriction/models/pos_config.py
@@ -4,46 +4,27 @@ from odoo import models, fields, api
 class PosConfig(models.Model):
     _inherit = 'pos.config'
 
-    # Use Integer instead of Many2one to avoid POS IndexedDB TypeError
-    # Many2one fields cause POS frontend to crash when loading model relations
-    allowed_warehouse_id = fields.Integer(
-        string='Linked Warehouse ID',
-        help='Internal ID of the warehouse linked to this POS for user restriction.',
-        default=0,
-    )
-
-    # Computed display field for the form view (not loaded by POS)
-    allowed_warehouse_display = fields.Many2one(
-        'stock.warehouse',
-        string='Linked Warehouse',
-        compute='_compute_allowed_warehouse_display',
-        inverse='_inverse_allowed_warehouse_display',
-        store=False,
-    )
-
-    def _compute_allowed_warehouse_display(self):
-        for rec in self:
-            if rec.allowed_warehouse_id:
-                rec.allowed_warehouse_display = self.env['stock.warehouse'].browse(
-                    rec.allowed_warehouse_id
-                )
-            else:
-                rec.allowed_warehouse_display = False
-
-    def _inverse_allowed_warehouse_display(self):
-        for rec in self:
-            rec.allowed_warehouse_id = rec.allowed_warehouse_display.id or 0
-
     @api.model
     def _search(self, domain, offset=0, limit=None, order=None, **kwargs):
         user = self.env.user
         if user._is_superuser():
             return super()._search(domain, offset=offset, limit=limit, order=order, **kwargs)
         if user.allowed_warehouse_ids:
-            allowed_wh_ids = user.allowed_warehouse_ids.ids
-            domain = list(domain) + [
-                ('allowed_warehouse_id', 'in', allowed_wh_ids)
-            ]
+            # Get warehouse names to match against POS config names
+            wh_names = user.allowed_warehouse_ids.mapped('name')
+            # Get POS config ids that match allowed warehouses by checking
+            # their picking types' warehouse
+            allowed_config_ids = []
+            all_configs = self.env['pos.config'].sudo().search([])
+            for config in all_configs:
+                # Check via picking type warehouse
+                if config.picking_type_id and config.picking_type_id.warehouse_id:
+                    if config.picking_type_id.warehouse_id.id in user.allowed_warehouse_ids.ids:
+                        allowed_config_ids.append(config.id)
+                else:
+                    allowed_config_ids.append(config.id)
+            if allowed_config_ids:
+                domain = list(domain) + [('id', 'in', allowed_config_ids)]
         return super()._search(domain, offset=offset, limit=limit, order=order, **kwargs)
 
 
@@ -56,8 +37,14 @@ class PosSession(models.Model):
         if user._is_superuser():
             return super()._search(domain, offset=offset, limit=limit, order=order, **kwargs)
         if user.allowed_warehouse_ids:
-            allowed_wh_ids = user.allowed_warehouse_ids.ids
-            domain = list(domain) + [
-                ('config_id.allowed_warehouse_id', 'in', allowed_wh_ids)
-            ]
+            allowed_config_ids = []
+            all_configs = self.env['pos.config'].sudo().search([])
+            for config in all_configs:
+                if config.picking_type_id and config.picking_type_id.warehouse_id:
+                    if config.picking_type_id.warehouse_id.id in user.allowed_warehouse_ids.ids:
+                        allowed_config_ids.append(config.id)
+                else:
+                    allowed_config_ids.append(config.id)
+            if allowed_config_ids:
+                domain = list(domain) + [('config_id', 'in', allowed_config_ids)]
         return super()._search(domain, offset=offset, limit=limit, order=order, **kwargs)
