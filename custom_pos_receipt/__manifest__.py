b4844fce32d59fa19f881075e1676c254254ddae
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-02 23:28:03 +0530

    custom

--- custom_pos_receipt/__manifest__.py
+++ custom_pos_receipt/__manifest__.py
@@ -1,19 +1,13 @@
-# -*- coding: utf-8 -*-
 {
     'name': 'Custom POS Receipt',
     'version': '19.0.1.0.0',
-    'summary': 'QR size reduction, GST table, amount in words, savings label, salutation',
+    'summary': 'Customize POS Receipt PDF without OWL JS',
     'category': 'Point of Sale',
     'author': 'Custom',
     'depends': ['point_of_sale'],
-    'assets': {
-        'point_of_sale._assets_pos': [
-            'custom_pos_receipt/static/src/utils/amount_in_words.js',
-            'custom_pos_receipt/static/src/overrides/order_receipt.js',
-            'custom_pos_receipt/static/src/overrides/order_receipt.xml',
-            'custom_pos_receipt/static/src/overrides/receipt_style.css',
-        ],
-    },
+    'data': [
+        'report/pos_receipt_template.xml',
+    ],
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
