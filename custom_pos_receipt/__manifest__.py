85b723d9f8019b32867f78883d1919e96fc8a275
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-02 23:07:35 +0530

    custom

--- custom_pos_receipt/__manifest__.py
+++ custom_pos_receipt/__manifest__.py
@@ -1,15 +1,20 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'POS Receipt Custom GST',
+    'name': 'Custom POS Receipt',
     'version': '19.0.1.0.0',
+    'summary': 'QR size reduction, GST table, amount in words, savings label, salutation',
     'category': 'Point of Sale',
+    'author': 'Custom',
     'depends': ['point_of_sale'],
-    'data': [],
     'assets': {
         'point_of_sale._assets_pos': [
-            'pos_receipt_custom/static/src/overrides/receipt.css',
-            'pos_receipt_custom/static/src/overrides/receipt.js',
+            'custom_pos_receipt/static/src/utils/amount_in_words.js',
+            'custom_pos_receipt/static/src/overrides/order_receipt.js',
+            'custom_pos_receipt/static/src/overrides/order_receipt.xml',
+            'custom_pos_receipt/static/src/overrides/receipt_style.css',
         ],
     },
     'installable': True,
+    'auto_install': False,
     'license': 'LGPL-3',
 }
