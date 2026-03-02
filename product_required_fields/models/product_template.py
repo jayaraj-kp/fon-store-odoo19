46a13c8e49d38a1315b6cb60e161bad23dc8b765
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-02 10:36:42 +0530

    custom

--- product_required_fields/models/product_template.py
+++ product_required_fields/models/product_template.py
@@ -5,29 +5,6 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    # Override fields to make them required at model level (handles UI + API)
+    # Override fields to make them required at model level (handles UI asterisk)
     default_code = fields.Char(required=True)
     barcode = fields.Char(required=True)
-
-    @api.constrains('default_code', 'barcode')
-    def _check_required_reference_barcode(self):
-        for record in self:
-            missing = []
-            if not record.default_code:
-                missing.append(_('Reference (Internal Reference)'))
-            if not record.barcode:
-                missing.append(_('Barcode'))
-            if missing:
-                raise ValidationError(
-                    _('The following required fields are missing: %s\n'
-                      'Please fill in all required fields before saving.')
-                    % ', '.join(missing)
-                )
-
-
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
-
-    # Also enforce on product.product (variant level)
-    barcode = fields.Char(required=True)
-    default_code = fields.Char(required=True)
