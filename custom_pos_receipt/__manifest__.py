eee02c2bcbdb84b69c23064a6900a2b146c31363
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-03 13:23:06 +0530

    odoomates

---
+++ custom_pos_receipt/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Custom POS Receipt',
+    'version': '19.0.1.0.0',
+    'summary': 'Advanced POS Receipt with Custom GST & Totals',
+    'category': 'Point of Sale',
+    'author': 'FON Store',
+    'depends': ['point_of_sale'],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'custom_pos_receipt/static/src/css/custom_receipt.css',
+            'custom_pos_receipt/static/src/js/pos_order_gst.js',
+            'custom_pos_receipt/static/src/xml/custom_receipt.xml',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
