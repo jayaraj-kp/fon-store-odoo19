411c61e063870e057ac5c73d30d777fcb0b4b528
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-03 23:29:57 +0530

    odoomates

--- custom_pos_receipt/static/src/js/pos_order_gst.js
+++ custom_pos_receipt/static/src/js/pos_order_gst.js
@@ -1,131 +1,22 @@
-///** @odoo-module **/
-//
-//import { PosOrder } from "@point_of_sale/app/models/pos_order";
-//import { patch } from "@web/core/utils/patch";
-//
-//patch(PosOrder.prototype, {
-//
-//    /* ================= GST BREAKDOWN ================= */
-//
-//    getGstBreakdown() {
-//        const grouped = {};
-//        const lines = this.lines || this.orderlines || [];
-//
-//        for (const line of lines) {
-//            const lineTaxes = line.tax_ids || [];
-//            const linePrice = line.price_subtotal || 0;
-//
-//            for (const tax of lineTaxes) {
-//                const rate = tax.amount || 0;
-//                const key = `rate_${rate}`;
-//
-//                if (!grouped[key]) {
-//                    grouped[key] = {
-//                        rate: rate,
-//                        label: rate === 0 ? "GST Exempt" : `GST @ ${rate}%`,
-//                        taxable: 0,
-//                        cgst: 0,
-//                        sgst: 0,
-//                    };
-//                }
-//
-//                grouped[key].taxable += linePrice;
-//                const taxAmount = linePrice * (rate / 100);
-//                grouped[key].cgst += taxAmount / 2;
-//                grouped[key].sgst += taxAmount / 2;
-//            }
-//        }
-//
-//        return Object.values(grouped).sort((a, b) => a.rate - b.rate);
-//    },
-//
-//    getTotalTaxableAmount() {
-//        const lines = this.lines || this.orderlines || [];
-//        return lines.reduce((sum, l) => sum + (l.price_subtotal || 0), 0);
-//    },
-//
-//    getTotalCgst() {
-//        return this.getGstBreakdown().reduce((sum, g) => sum + g.cgst, 0);
-//    },
-//
-//    getTotalSgst() {
-//        return this.getGstBreakdown().reduce((sum, g) => sum + g.sgst, 0);
-//    },
-//
-//    /* ================= CUSTOM TOTAL SECTION ================= */
-//
-//    getBeforeGrandTotal() {
-//        return this.getTotalTaxableAmount();
-//    },
-//
-//    getGrandTotal() {
-//        return this.amount_total || 0;
-//    },
-//
-//    getTotalSaved() {
-//        const lines = this.lines || this.orderlines || [];
-//        let totalSaved = 0;
-//
-//        for (const line of lines) {
-//            const qty = line.qty || 0;
-//            const unitPrice = line.price_unit || 0;
-//            const discount = line.discount || 0;
-//
-//            const lineTotal = unitPrice * qty;
-//            const discountAmount = lineTotal * (discount / 100);
-//            totalSaved += discountAmount;
-//        }
-//
-//        return totalSaved;
-//    },
-//
-//    /* ================= AMOUNT IN WORDS ================= */
-//
-//    getAmountInWords() {
-//        const amount = Math.floor(this.getGrandTotal());
-//
-//        const words = [
-//            "Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten",
-//            "Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen",
-//            "Eighteen","Nineteen"
-//        ];
-//
-//        const tens = [
-//            "","","Twenty","Thirty","Forty","Fifty",
-//            "Sixty","Seventy","Eighty","Ninety"
-//        ];
-//
-//        function convert(n) {
-//            if (n < 20) return words[n];
-//            if (n < 100)
-//                return tens[Math.floor(n/10)] + " " + words[n%10];
-//            if (n < 1000)
-//                return words[Math.floor(n/100)] + " Hundred " + convert(n%100);
-//            return n;
-//        }
-//
-//        return convert(amount) + " Only";
-//    },
-//
-//});
-
 /** @odoo-module **/
 
 import { patch } from "@web/core/utils/patch";
-import { OrderReceipt } from "point_of_sale.models";
+import { PosOrder } from "@point_of_sale/app/models/pos_order";
 
-patch(OrderReceipt.prototype, "custom_pos_receipt_patch", {
+patch(PosOrder.prototype, 'custom_receipt_patch', {
 
-    // GST breakdown
     getGstBreakdown() {
         const grouped = {};
-        const lines = this.lines || this.orderlines || [];
+        const lines = this.orderlines.models || [];
+
         for (const line of lines) {
             const lineTaxes = line.tax_ids || [];
             const linePrice = line.price_subtotal || 0;
+
             for (const tax of lineTaxes) {
                 const rate = tax.amount || 0;
                 const key = `rate_${rate}`;
+
                 if (!grouped[key]) {
                     grouped[key] = {
                         rate: rate,
@@ -135,18 +26,19 @@ patch(OrderReceipt.prototype, "custom_pos_receipt_patch", {
                         sgst: 0,
                     };
                 }
+
                 grouped[key].taxable += linePrice;
                 const taxAmount = linePrice * (rate / 100);
                 grouped[key].cgst += taxAmount / 2;
                 grouped[key].sgst += taxAmount / 2;
             }
         }
+
         return Object.values(grouped).sort((a, b) => a.rate - b.rate);
     },
 
     getTotalTaxableAmount() {
-        const lines = this.lines || this.orderlines || [];
-        return lines.reduce((sum, l) => sum + (l.price_subtotal || 0), 0);
+        return (this.orderlines.models || []).reduce((sum, l) => sum + (l.price_subtotal || 0), 0);
     },
 
     getTotalCgst() {
@@ -166,14 +58,10 @@ patch(OrderReceipt.prototype, "custom_pos_receipt_patch", {
     },
 
     getTotalSaved() {
-        const lines = this.lines || this.orderlines || [];
         let totalSaved = 0;
-        for (const line of lines) {
-            const qty = line.qty || 0;
-            const unitPrice = line.price_unit || 0;
-            const discount = line.discount || 0;
-            const lineTotal = unitPrice * qty;
-            const discountAmount = lineTotal * (discount / 100);
+        for (const line of (this.orderlines.models || [])) {
+            const lineTotal = (line.price_unit || 0) * (line.qty || 0);
+            const discountAmount = lineTotal * ((line.discount || 0) / 100);
             totalSaved += discountAmount;
         }
         return totalSaved;
@@ -181,18 +69,26 @@ patch(OrderReceipt.prototype, "custom_pos_receipt_patch", {
 
     getAmountInWords() {
         const amount = Math.floor(this.getGrandTotal());
+
         const words = [
             "Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten",
             "Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen",
             "Eighteen","Nineteen"
         ];
-        const tens = ["","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"];
-        function convert(n){
-            if(n < 20) return words[n];
-            if(n < 100) return tens[Math.floor(n/10)] + " " + words[n%10];
-            if(n < 1000) return words[Math.floor(n/100)] + " Hundred " + convert(n%100);
+
+        const tens = [
+            "","","Twenty","Thirty","Forty","Fifty",
+            "Sixty","Seventy","Eighty","Ninety"
+        ];
+
+        function convert(n) {
+            if (n < 20) return words[n];
+            if (n < 100) return tens[Math.floor(n/10)] + " " + words[n%10];
+            if (n < 1000) return words[Math.floor(n/100)] + " Hundred " + convert(n%100);
             return n;
         }
+
         return convert(amount) + " Only";
     },
+
 });
