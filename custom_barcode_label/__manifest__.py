5f95ec48d24539464a33a3781ec35656f1c25f67
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-04 15:18:19 +0530

    odoomates

---
+++ custom_barcode_label/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Custom Barcode Label',
+    'version': '19.0.1.0.0',
+    'summary': 'Customize product barcode label: add company logo, remove code/barcode numbers, reposition price and name',
+    'author': 'Your Company',
+    'category': 'Inventory',
+    'depends': ['stock', 'product'],
+    'data': [
+        'report/product_label_report.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
