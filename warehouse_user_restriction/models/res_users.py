e65b14056a4759f1b21d2e363cbb340ea92739ea
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-05 10:29:33 +0530

    custom

--- warehouse_user_restriction/models/res_users.py
+++ warehouse_user_restriction/models/res_users.py
@@ -23,11 +23,20 @@ class ResUsers(models.Model):
         string='Default Customer Tags',
     )
 
+    disallowed_product_category_ids = fields.Many2many(
+        'product.category',
+        'res_users_disallowed_product_category_rel',
+        'user_id',
+        'category_id',
+        string='Disallowed Product Categories',
+    )
+
     @property
     def SELF_READABLE_FIELDS(self):
         return super().SELF_READABLE_FIELDS + [
             'allowed_warehouse_ids',
             'default_customer_tag_ids',
+            'disallowed_product_category_ids',
         ]
 
     @api.model
@@ -37,3 +46,46 @@ class ResUsers(models.Model):
         safe = ['id', 'name', 'partner_id', 'all_group_ids']
         _logger.info("WHR_DEBUG returning safe fields: %s", safe)
         return safe
+
+
+
+
+# import logging
+# from odoo import models, fields, api
+#
+# _logger = logging.getLogger(__name__)
+#
+#
+# class ResUsers(models.Model):
+#     _inherit = 'res.users'
+#
+#     allowed_warehouse_ids = fields.Many2many(
+#         'stock.warehouse',
+#         'res_users_stock_warehouse_rel',
+#         'user_id',
+#         'warehouse_id',
+#         string='Allowed Warehouses',
+#     )
+#
+#     default_customer_tag_ids = fields.Many2many(
+#         'res.partner.category',
+#         'res_users_partner_category_rel',
+#         'user_id',
+#         'category_id',
+#         string='Default Customer Tags',
+#     )
+#
+#     @property
+#     def SELF_READABLE_FIELDS(self):
+#         return super().SELF_READABLE_FIELDS + [
+#             'allowed_warehouse_ids',
+#             'default_customer_tag_ids',
+#         ]
+#
+#     @api.model
+#     def _load_pos_data_fields(self, config):
+#         result = super()._load_pos_data_fields(config)
+#         _logger.info("WHR_DEBUG _load_pos_data_fields called, original fields: %s", result)
+#         safe = ['id', 'name', 'partner_id', 'all_group_ids']
+#         _logger.info("WHR_DEBUG returning safe fields: %s", safe)
+#         return safe
