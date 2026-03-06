8b79440c18b8558f2a613ab0835024d80e76c1f8
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-06 11:11:55 +0530

    odoomates

--- pos_cash_customer/models/res_config_settings.py
+++ pos_cash_customer/models/res_config_settings.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ResConfigSettings(models.TransientModel):
@@ -7,7 +7,25 @@ class ResConfigSettings(models.TransientModel):
     pos_cash_customer_id = fields.Many2one(
         'res.partner',
         string='POS Cash Customer',
-        config_parameter='pos_cash_customer.cash_customer_id',
-        # Removed domain restriction - allow any partner (company or individual)
-        help='The master CASH CUSTOMER partner. All new POS customers will be added as contacts under this partner.',
+        help='The master CASH CUSTOMER partner. All new POS customers will be added as contacts under this partner. Leave empty to use the standard create customer form.',
     )
+
+    def set_values(self):
+        super().set_values()
+        self.env['ir.config_parameter'].sudo().set_param(
+            'pos_cash_customer.cash_customer_id',
+            self.pos_cash_customer_id.id or 0
+        )
+
+    @api.model
+    def get_values(self):
+        res = super().get_values()
+        param = self.env['ir.config_parameter'].sudo().get_param(
+            'pos_cash_customer.cash_customer_id', 0
+        )
+        try:
+            partner_id = int(param)
+        except (ValueError, TypeError):
+            partner_id = 0
+        res['pos_cash_customer_id'] = partner_id or False
+        return res
