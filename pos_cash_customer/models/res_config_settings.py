f55971e550016d02b9362870408ab31084156ee5
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-09 09:18:14 +0530

    odoomates

--- pos_cash_customer/models/res_config_settings.py
+++ pos_cash_customer/models/res_config_settings.py
@@ -12,20 +12,22 @@ class ResConfigSettings(models.TransientModel):
 
     def set_values(self):
         super().set_values()
+        # Use the partner's ID; if no partner selected, store empty string
+        partner_id = self.pos_cash_customer_id.id
         self.env['ir.config_parameter'].sudo().set_param(
             'pos_cash_customer.cash_customer_id',
-            self.pos_cash_customer_id.id or 0
+            str(partner_id) if partner_id else ''
         )
 
     @api.model
     def get_values(self):
         res = super().get_values()
         param = self.env['ir.config_parameter'].sudo().get_param(
-            'pos_cash_customer.cash_customer_id', 0
+            'pos_cash_customer.cash_customer_id', ''
         )
         try:
-            partner_id = int(param)
+            partner_id = int(param) if param else 0
         except (ValueError, TypeError):
             partner_id = 0
-        res['pos_cash_customer_id'] = partner_id or False
+        res['pos_cash_customer_id'] = partner_id if partner_id else False
         return res
