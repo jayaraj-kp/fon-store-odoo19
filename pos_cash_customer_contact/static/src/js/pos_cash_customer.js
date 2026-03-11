335ec289dcac4b5e349e15d55a1e2845312a8d6f
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-11 23:05:48 +0530

    custom

--- pos_cash_customer_contact/static/src/js/pos_cash_customer.js
+++ pos_cash_customer_contact/static/src/js/pos_cash_customer.js
@@ -1,34 +1,21 @@
 /** @odoo-module **/
 
 import { patch } from "@web/core/utils/patch";
-import { CustomerListScreen } from "@point_of_sale/app/screens/customer_list/customer_list_screen";
+import { PosStore } from "@point_of_sale/app/store/pos_store";
 
-patch(CustomerListScreen.prototype, {
+patch(PosStore.prototype, {
 
-    async showCreateCustomer() {
+    async createPartner(vals) {
 
-        const cashCustomer = this.pos.models["res.partner"].find(
+        const cashCustomer = this.models["res.partner"].find(
             (p) => p.name === "CASH CUSTOMER"
         );
 
-        let parent_id = cashCustomer ? cashCustomer.id : false;
-
-        const partner = {
-            parent_id: parent_id,
-        };
-
-        const { confirmed, payload } = await this.editPartner(partner);
-
-        if (confirmed) {
-
-            payload.parent_id = parent_id;
-
-            const newPartner = await this.pos.data.create("res.partner", [payload]);
-
-            await this.pos.data.loadPartners();
-
-            this.props.resolve(newPartner);
+        if (cashCustomer) {
+            vals.parent_id = cashCustomer.id;
         }
-    }
+
+        return await super.createPartner(vals);
+    },
 
 });
