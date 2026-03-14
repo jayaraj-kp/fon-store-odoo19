32a4f7905ec2ddda0543ba1eafeb19af93d2ebcb
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-14 10:44:49 +0530

    custom

--- pos_payment_method_column/__manifest__.py
+++ pos_payment_method_column/__manifest__.py
@@ -3,17 +3,14 @@
     'version': '19.0.1.0.0',
     'category': 'Point of Sale',
     'summary': 'Adds Payment Method column to POS Orders list',
-    'description': """
-        Adds a Payment Method column to the Point of Sale Orders list screen,
-        showing how each order was paid (Cash, Card, etc.).
-    """,
     'author': 'Custom',
     'depends': ['point_of_sale'],
     'data': [],
     'assets': {
         'point_of_sale._assets_pos': [
-            'pos_payment_method_column/static/src/js/OrderRow.js',
-            'pos_payment_method_column/static/src/xml/OrderRow.xml',
+            'pos_payment_method_column/static/src/js/order_row_patch.js',
+            'pos_payment_method_column/static/src/xml/order_row_patch.xml',
+            'pos_payment_method_column/static/src/css/order_row_patch.css',
         ],
     },
     'installable': True,
