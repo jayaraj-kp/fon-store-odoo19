947680a942b7c960ebda39cbc3dadd3fa8115af3
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-14 15:21:01 +0530

    custom

---
+++ base_sparse_field/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Base Sparse Field",
+    "summary": "Sparse fields support",
+    "version": "19.0.1.0.0",
+    "category": "Technical",
+    "author": "OCA, Odoo Community Association",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "installable": True,
+}
