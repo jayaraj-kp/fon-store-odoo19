e6dcc9119e237a474818bd9882d5be3768f127de
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-17 16:14:13 +0530

    custom

--- add_bank_account_menu/__manifest__.py
+++ add_bank_account_menu/__manifest__.py
@@ -3,7 +3,10 @@
     "version": "19.0.1.0.0",
     "summary": "Adds 'Add a Bank Account' menu under Configuration > Banks",
     "depends": ["account"],
-    "data": ["views/menu.xml"],
+    "data": [
+        # "views/menu.xml"
+        "views/bank_statement_button.xml",
+    ],
     "installable": True,
     "auto_install": False,
 }
