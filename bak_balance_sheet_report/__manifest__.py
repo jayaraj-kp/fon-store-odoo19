f766d287bcf009751c46b9dbfc6998330e08707c
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-18 17:34:58 +0530

    custom

--- bak_balance_sheet_report/__manifest__.py
+++ bak_balance_sheet_report/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Balance Sheet Inline Report',
-    'version': '17.0.1.0.0',
+    'version': '19.0.1.0.0',
     'category': 'Accounting',
     'summary': 'Renders Balance Sheet as an inline browser report with PDF/XLSX export',
     'description': """
