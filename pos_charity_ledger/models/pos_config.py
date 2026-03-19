15b029e5ea409fc03d4232d5c52cc8c9b7f16bea
jayaraj-kp <jayarajkprajkp@gmail.com>
2026-03-19 10:28:01 +0000

    c

--- pos_charity_ledger/models/pos_config.py
+++ pos_charity_ledger/models/pos_config.py
@@ -5,28 +5,19 @@ from odoo import models, fields
 class PosConfig(models.Model):
     _inherit = 'pos.config'
 
-    charity_enabled = fields.Boolean(
-        string='Enable Charity Donations',
-        default=False,
-    )
+    charity_enabled = fields.Boolean(string='Enable Charity Donations', default=False)
     charity_account_id = fields.Many2one(
-        'pos.charity.account',
-        string='Charity Account',
+        'pos.charity.account', string='Charity Account',
         domain=[('active', '=', True)],
     )
-    charity_button_label = fields.Char(
-        string='Charity Button Label',
-        default='Donate to Charity',
-    )
+    charity_button_label = fields.Char(string='Charity Button Label', default='Donate to Charity')
     charity_gl_account_id = fields.Many2one(
         'account.account',
         string='Charity GL Account',
-        help='The accounting account where charity donations will be credited (e.g. Charity Payable). '
-             'The debit side will use the POS cash/payment journal account.',
-        domain=[('deprecated', '=', False)],
+        help='The accounting account where charity donations will be credited (e.g. 201002 Charity).',
     )
     charity_journal_id = fields.Many2one(
         'account.journal',
         string='Charity Journal',
-        help='Journal to use for charity donation journal entries. Leave empty to use the POS sales journal.',
+        help='Journal for charity entries. Leave empty to use POS Sales Journal.',
     )
