e69b5584f2c8ccbf62f4fc16d5f9a2ce1410f86a
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-28 15:50:39 +0530

    c

---
+++ pos_hide_customer/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'POS Hide Customer Button',
+    'version': '19.0.1.0.0',
+    'category': 'Point of Sale',
+    'summary': 'Hides the Customer button in POS order screen',
+    'author': 'Custom',
+    'depends': ['point_of_sale'],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_hide_customer/static/src/js/hide_customer_button.js',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
