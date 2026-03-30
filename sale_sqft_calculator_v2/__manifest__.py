e1ebd1c7dacbc767f26a099e952a33fc13d07bb6
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-30 17:04:12 +0530

    c

--- sale_sqft_calculator_v2/__manifest__.py
+++ sale_sqft_calculator_v2/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Sale Square Feet Calculator',
-    'version': '19.0.1.0.0',
+    'version': '19.0.2.0.0',
     'category': 'Sales',
     'summary': 'Calculate product quantity based on Width x Height (sq.ft)',
     'author': 'TJ Ardor Creations',
