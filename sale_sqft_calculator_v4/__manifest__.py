4616eb817dc7cacabe3dcc072272c717721d15ca
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-03-31 12:18:57 +0530

    c

--- sale_sqft_calculator_v4/__manifest__.py
+++ sale_sqft_calculator_v4/__manifest__.py
@@ -1,22 +1,15 @@
 {
     'name': 'Sale Square Feet Calculator',
-    'version': '19.0.3.0.0',
+    'version': '19.0.4.0.0',
     'category': 'Sales',
     'summary': 'Calculate product quantity based on Width x Height (sq.ft)',
     'author': 'TJ Ardor Creations',
-    'depends': ['sale_management', 'web'],
+    'depends': ['sale_management'],
     'data': [
         'security/ir.model.access.csv',
         'views/sqft_wizard_views.xml',
         'views/sale_order_views.xml',
     ],
-    'assets': {
-        'web.assets_backend': [
-            # OWL XML template must come BEFORE the JS component
-            'sale_sqft_calculator/static/src/xml/sqft_calculator_button.xml',
-            'sale_sqft_calculator/static/src/js/sqft_calculator_button.js',
-        ],
-    },
     'installable': True,
     'application': False,
     'license': 'LGPL-3',
