70b03c338616f80ccb4309518adb48eb97e088fd
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-04-01 14:00:35 +0530

    c

--- enterprise_theme_mod/__manifest__.py
+++ enterprise_theme_mod/__manifest__.py
@@ -1,31 +1,19 @@
 {
     "name": "Odoo Enterprise Theme",
-    "version": "19.0.0.1",
+    "version": "19.0.0.2",
     "summary": "Enterprise-style theme for Odoo 19 Community Edition",
     "author": "fl1 sro",
     "license": "AGPL-3",
-    "maintainer": "Fl1",
-    "company": "Fl1 sro",
-    "website": "https://fl1.cz",
-    "depends": [
-        "web",
-    ],
+    "depends": ["web"],
     "category": "Branding",
-    "description": """
-        Enterprise-style Theme for Odoo 19 Community Edition.
-        Provides a purple/mauve home menu background with colored app icons,
-        matching the Enterprise look and feel.
-    """,
     "assets": {
-        "web._assets_primary_variables": [
-            "/enterprise_theme_mod/static/src/scss/primary_variables_custom.scss",
-        ],
         "web.assets_backend": [
             "/enterprise_theme_mod/static/src/scss/backend_theme.scss",
         ],
+        "web.assets_web": [
+            "/enterprise_theme_mod/static/src/scss/backend_theme.scss",
+        ],
     },
-    "price": 0,
-    "currency": "EUR",
     "installable": True,
     "auto_install": False,
     "application": True,
