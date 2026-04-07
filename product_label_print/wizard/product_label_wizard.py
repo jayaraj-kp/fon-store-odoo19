eef35b036fe77ac67071b20727b37487a33709ab
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-04-07 15:26:33 +0530

    c

--- product_label_print/wizard/product_label_wizard.py
+++ product_label_print/wizard/product_label_wizard.py
@@ -885,7 +885,7 @@ class ProductLabelWizard(models.TransientModel):
         LH      = QR_H + BOT_H
         QR_SIZE = 28
         COL_GAP = 60
-        ROW_GAP = 4
+        ROW_GAP = 1
         L_MAR   = 9
         PW      = 155
 
