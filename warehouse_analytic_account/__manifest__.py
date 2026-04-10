46def533b2abb6d8692027bfda7d4f63d356410a
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-04-10 14:22:21 +0530

    c

--- warehouse_analytic_account/__manifest__.py
+++ warehouse_analytic_account/__manifest__.py
@@ -1,45 +1,9 @@
-# # -*- coding: utf-8 -*-
-# {
-#     'name': 'Warehouse Analytic Account',
-#     'version': '19.0.2.0.0',
-#     'category': 'Inventory/Configuration',
-#     'summary': 'Auto-apply warehouse analytic account on sales, purchases and invoices',
-#     'description': """
-#         Adds an Analytic Account field on each Warehouse.
-#         When a user whose default warehouse has an analytic account creates any of
-#         the following documents, the analytic account is automatically applied
-#         to all order/invoice lines:
-#
-#         - Sales Quotation & Sales Order
-#         - Purchase Quotation (RFQ) & Purchase Order
-#         - Customer Invoice & Credit Note
-#         - Vendor Bill & Vendor Refund
-#         - Stock moves / pickings (on validation)
-#     """,
-#     'author': 'Custom',
-#     'depends': [
-#         'stock',
-#         'purchase',
-#         'sale',
-#         'account',
-#         'analytic',
-#     ],
-#     'data': [
-#         'security/ir.model.access.csv',
-#         'views/stock_warehouse_views.xml',
-#         'views/res_users_views.xml',
-#     ],
-#     'installable': True,
-#     'application': False,
-#     'auto_install': False,
-#     'license': 'LGPL-3',
-# }
 # -*- coding: utf-8 -*-
 {
     'name': 'Warehouse Analytic Account',
-    'version': '19.0.3.0.0',
+    'version': '19.0.2.0.0',
     'category': 'Inventory/Configuration',
-    'summary': 'Auto-apply warehouse analytic account on sales, purchases, invoices and POS orders',
+    'summary': 'Auto-apply warehouse analytic account on sales, purchases and invoices',
     'description': """
         Adds an Analytic Account field on each Warehouse.
         When a user whose default warehouse has an analytic account creates any of
@@ -51,7 +15,6 @@
         - Customer Invoice & Credit Note
         - Vendor Bill & Vendor Refund
         - Stock moves / pickings (on validation)
-        - POS Orders & POS Order Lines (NEW)
     """,
     'author': 'Custom',
     'depends': [
@@ -60,14 +23,11 @@
         'sale',
         'account',
         'analytic',
-        'point_of_sale',  # Added POS dependency
     ],
     'data': [
         'security/ir.model.access.csv',
         'views/stock_warehouse_views.xml',
         'views/res_users_views.xml',
-
-
     ],
     'installable': True,
     'application': False,
