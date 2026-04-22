7f86f3889409715cde3770931654bfdfd0c449c9
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-04-22 14:32:59 +0530

    custom

---
+++ purchase_order_required/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'vendor Reference required',
+    'version': '19.0.0.1.0.0',
+    'summary': 'Makes vendor Reference required ',
+    # 'category': 'Inventory',
+    'author': 'Custom',
+    'depends': ['purchase'],
+    # 'data': [
+    #     'views/product_template_views.xml',
+    # ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ purchase_order_required/models/purchase_order.py
@@ -0,0 +1,7 @@
+from odoo import models , fields , api , _
+from odoo.exceptions import ValidationError
+
+class PurchaseOrder(models.Model):
+     _inherit="purchase_order"
+
+     partner_ref= fields.char(required=True)
