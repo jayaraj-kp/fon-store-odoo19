9749f78077adaae21df9089294f67c1141cea3b6
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-04-22 15:04:31 +0530

    custom

--- vendor_ref_required/hooks.py
+++ vendor_ref_required/hooks.py
@@ -1,4 +1,7 @@
 def post_init_hook(env):
     """Set date format to dd/mm/yyyy for all installed languages."""
     langs = env['res.lang'].search([])
-    langs.write({'date_format': '%d/%m/%Y'})
+    langs.write({
+        'date_format': '%d/%m/%Y',
+        'time_format': '%H:%M:%S',
+    })
