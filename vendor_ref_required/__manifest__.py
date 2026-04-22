1b66ee924db8d07c055c81b3aff87fb78f380c6f
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-04-22 15:44:42 +0530

    custom

--- vendor_ref_required/__manifest__.py
+++ vendor_ref_required/__manifest__.py
@@ -1,13 +1,13 @@
 {
     'name': 'Vendor Reference Required',
     'version': '1.0',
-    'summary': 'Makes Vendor Reference required and displays dates in dd/mm/yyyy format without time',
+    'summary': 'Makes Vendor Reference required ',
     'author': 'Custom',
     'depends': ['purchase'],
-    'data': [
-        'views/purchase_order_views.xml',
-    ],
-    'post_init_hook': 'post_init_hook',
+    # 'data': [
+    #     'views/purchase_order_views.xml',
+    # ],
+
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
