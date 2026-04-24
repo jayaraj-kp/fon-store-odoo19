65fbadcbfee054437494646dc3397f2f477b2c63
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-04-24 10:46:32 +0530

    custom

--- periodic_valuation_report/__manifest__.py
+++ periodic_valuation_report/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Periodic Inventory Valuation Report',
     'version': '1.0',
-    'depends': ['stock', 'purchase'],
+    'depends': ['stock', 'purchase', 'account'],
     'data': [
         'security/ir.model.access.csv',
         'views/report_view.xml',
