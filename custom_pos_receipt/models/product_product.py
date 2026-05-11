f354f15e0731e853f6fdad97b3e416a5b9c18229
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-05-11 15:46:53 +0530

    custom

--- custom_pos_receipt/models/product_product.py
+++ custom_pos_receipt/models/product_product.py
@@ -1,5 +1,16 @@
 from odoo import models
 
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    def _load_pos_data_fields(self, config_id):
+        fields = super()._load_pos_data_fields(config_id)
+        if 'mrp_price' not in fields:
+            fields.append('mrp_price')
+        return fields
+
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
