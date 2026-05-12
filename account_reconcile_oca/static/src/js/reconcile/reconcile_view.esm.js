2a42b775b765a9f03dfb18c5e29d0cd69b298098
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-05-12 15:53:50 +0530

    custom

--- account_reconcile_oca/static/src/js/reconcile/reconcile_view.esm.js
+++ account_reconcile_oca/static/src/js/reconcile/reconcile_view.esm.js
@@ -1,17 +1,3 @@
-//import {ReconcileController} from "./reconcile_controller.esm.js";
-//import {ReconcileRenderer} from "./reconcile_renderer.esm.js";
-//import {kanbanView} from "@web/views/kanban/kanban_view";
-//import {registry} from "@web/core/registry";
-//
-//export const reconcileView = {
-//    ...kanbanView,
-//    Renderer: ReconcileRenderer,
-//    Controller: ReconcileController,
-//    buttonTemplate: "account_reconcile.ReconcileView.Buttons",
-//    searchMenuTypes: ["filter"],
-//};
-//
-//registry.category("views").add("reconcile", reconcileView);
 import {ReconcileController} from "./reconcile_controller.esm.js";
 import {ReconcileRenderer} from "./reconcile_renderer.esm.js";
 import {kanbanView} from "@web/views/kanban/kanban_view";
@@ -26,4 +12,3 @@ export const reconcileView = {
 };
 
 registry.category("views").add("reconcile", reconcileView);
-
