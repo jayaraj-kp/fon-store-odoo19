6b6bd13c83f0e0ccd6a2e484af45b460b4608952
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-05-15 16:58:08 +0530

    custom

--- favorites/__manifest__.py
+++ favorites/__manifest__.py
@@ -4,9 +4,10 @@
     'category': 'Web',
     'summary': 'Remove the Favorites menu from the search bar',
     'description': 'This module removes the Favorites tab/menu from the search bar dropdown.',
-    'depends': ['web'],
+    'depends': ['web', 'account'],
     'data': [
         'data/data.xml',
+        'views/actions.xml',
     ],
     'assets': {
         'web.assets_backend': [
