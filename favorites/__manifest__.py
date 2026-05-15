d3453bcf54780e176501b45604ad0f4d261af37e
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-05-15 16:39:23 +0530

    custom

---
+++ favorites/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Remove Search Favorites',
+    'version': '1.0',
+    'category': 'Web',
+    'summary': 'Remove the Favorites menu from the search bar',
+    'description': 'This module removes the Favorites tab/menu from the search bar dropdown.',
+    'depends': ['web'],
+    'data': [
+        'data/data.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'favorites/static/src/xml/favorites.xml',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
