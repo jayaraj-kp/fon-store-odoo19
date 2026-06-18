0776747068070daa1da531169e974b83b056b073
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-06-18 16:43:48 +0530

    cu

--- internal_journal_entry/__init__.py
+++ internal_journal_entry/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 from . import models
-from . import hooks
+# Import function directly so Odoo can find it as a root-level attribute:
+# odoo.addons.internal_journal_entry.post_init_hook
+from .hooks import post_init_hook
