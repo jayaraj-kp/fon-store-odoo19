70cf1a5124362e50b734bd97e9fdd08650ded3f2
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-07-04 11:52:38 +0530

    cu

--- pos_cash_customer_contact/static/src/js/phone_customer_bar.js
+++ pos_cash_customer_contact/static/src/js/phone_customer_bar.js
@@ -1003,7 +1003,6 @@ export class CreateCustomerDialog extends Component {
             name: query,
             phone: isPhone ? query : "",
             email: "",
-            gender: "male",
             saving: false,
             error: "",
             tagId: this.props.autoTag ? this.props.autoTag.id : false,
@@ -1101,7 +1100,6 @@ export class CreateCustomerDialog extends Component {
                 phone: this.form.phone.trim(),
                 email: this.form.email.trim(),
                 tagId: this.form.tagId,
-                gender: this.form.gender,
             });
 
             console.log("✅ onCreated succeeded, closing dialog");
@@ -1447,7 +1445,6 @@ export class PhoneCustomerBar extends Component {
             email: formData.email || false,
             parent_id: parentId,
             customer_rank: 1,
-            gender: formData.gender || false,
         };
 
         // Attach the contact tag if resolved
