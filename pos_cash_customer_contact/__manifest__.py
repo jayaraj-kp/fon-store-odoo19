bf196fedf6a49cd54897eff68e8fa6af2f5a768d
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-07-06 11:16:37 +0530

    cu

--- pos_cash_customer_contact/__manifest__.py
+++ pos_cash_customer_contact/__manifest__.py
@@ -1,7 +1,21 @@
+# {
+#     "name": "POS Cash Customer Contact",
+#     "version": "2.0",
+#     "depends": ["point_of_sale"],
+#     "assets": {
+#         "point_of_sale._assets_pos": [
+#             "pos_cash_customer_contact/static/src/css/phone_customer_bar.css",
+#             "pos_cash_customer_contact/static/src/js/phone_customer_bar.js",
+#             "pos_cash_customer_contact/static/src/xml/phone_customer_bar.xml",
+#         ],
+#     },
+#     "installable": True,
+# }
+
 {
     "name": "POS Cash Customer Contact",
     "version": "2.0",
-    "depends": ["point_of_sale"],
+    "depends": ["point_of_sale", "partner_gender"],
     "assets": {
         "point_of_sale._assets_pos": [
             "pos_cash_customer_contact/static/src/css/phone_customer_bar.css",
